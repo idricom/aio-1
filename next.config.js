4b3fc3a23d4a651fec62947ea169a7cf549f33c4
idricom <160354973+idricom@users.noreply.github.com>
2025-11-10 17:01:39 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: { appDir: false },
-  async rewrites() {
-    return [
-      // optional rewrites if needed
-    ];
-  }
+  swcMinify: true
 }
+
 module.exports = nextConfig
